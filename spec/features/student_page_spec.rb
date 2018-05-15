7d983e006fd7d252701fd6663c31ae123456ac3a
Sabrina Robinson <sabrinarob@gmail.com>
2018-05-15 13:53:43 -0600

    Adding index and new to conroller

--- spec/features/student_page_spec.rb
+++ spec/features/student_page_spec.rb
@@ -10,6 +10,7 @@ describe "user sees all students" do
       visit students_path
 
       expect(current_path).to eq(students_path)
+      # save_and_open_page
       expect(current_path).to have_content(student_1.name)
       expect(current_path).to have_content(student_2.name)
     end

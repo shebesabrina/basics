33d4a6394312666debe77ce32e6ea4c3fd911163
Sabrina Robinson <sabrinarob@gmail.com>
2018-05-15 14:46:20 -0600

    Creating feature test for create student feature.

--- spec/features/student_page_spec.rb
+++ spec/features/student_page_spec.rb
@@ -1,5 +1,9 @@
 require "rails_helper"
 
+# As a user
+# When I visit `/students/:id`
+# I see the name of a student
+
 describe 'user sees one student' do
   describe 'they visit/student/:id' do
     it 'displays one student' do
@@ -13,6 +17,10 @@ describe 'user sees one student' do
   end
 end
 
+# As a user
+# When I visit `/students`
+# I see a list of student names
+
 describe "user sees all students" do
   describe "they visit /students" do
     it "displays all students" do
@@ -25,6 +33,35 @@ describe "user sees all students" do
       expect(current_path).to eq(students_path)
       expect(current_path).to have_content(student_1.name)
       expect(current_path).to have_content(student_2.name)
+    end
+  end
+end
+
+# As a user
+# When I visit `/students/new`
+# And I fill in name
+# And I click submit
+# I am on the student show page
+# And I see that student's name
+
+describe 'when user visits /students/new' do
+  describe 'they fill in a name and click submit' do
+    describe 'user sees their name on student show page' do
+      xit "displays new student name" do
+          student_1 = Student.create!(name: 'Ian')
+          student_2 = Student.create!(name: 'Dione')
+
+          new_name = 'Sabrina'
+
+          expect(current_path).to eq(new_student_path)
+
+          fill_in 'student[name]', with: new_name
+
+          click_on 'submit'
+
+          expect(current_path).to eq(student_path)
+          expect(page).to have_content(new_name)
+      end
     end
   end
 end

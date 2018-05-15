a0c1229f498adca8a4f4f0e075765001ef138ab0
Sabrina Robinson <sabrinarob@gmail.com>
2018-05-15 13:45:10 -0600

    Adding feature test to see all students

---
+++ spec/features/student_page_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+describe "user sees all students" do
+  describe "they visit /students" do
+    it "displays all students" do
+
+      student_1 = Student.create!(name: 'Ian')
+      student_2 = Student.create!(name: 'Dione')
+
+      visit students_path
+
+      expect(current_path).to eq(students_path)
+      expect(current_path).to have_content(student_1.name)
+      expect(current_path).to have_content(student_2.name)
+    end
+  end
+end
